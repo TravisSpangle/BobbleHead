1b2fc17ce437160026cea6787f4053ff237204d1
Travis Spangle <tspangle@Travis-Spangles-MacBook.local>
2011-01-12 21:22:18 -0800

    Initial project.

---
+++ Classes/BobbleHeadAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  BobbleHeadAppDelegate.h
+//  BobbleHead
+//
+//  Created by Travis Spangle on 1/12/11.
+//  Copyright 2011 Peak Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BobbleHeadAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
